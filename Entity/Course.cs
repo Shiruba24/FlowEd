ff4a8038dab08f5e9120f8104b24711d64b5d2a1
Shiruba24 <rust2435@gmail.com>
2024-05-04 21:22:42 +0500

    adding Course entity

---
+++ Entity/Course.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public float Price { get; set; }
+        public string Instructor { get; set; } = string.Empty;
+        public decimal Rating { get; set; }
+        public string Image { get; set; } = string.Empty;
+    }
+}
