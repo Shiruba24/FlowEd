8dfa922802319cf576308f36d1d90c1a7165fb3e
Shiruba24 <rust2435@gmail.com>
2024-05-06 10:27:55 +0500

    add react-router and sass

--- API/Program.cs
+++ API/Program.cs
@@ -8,13 +8,11 @@ internal class Program
     {
         var builder = WebApplication.CreateBuilder(args);
         var configuration = builder.Configuration;
+        var services = builder.Services;
         // Add services to the container.
-        builder.Services.AddControllers();
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddAuthorization();
-        builder.Services.AddAuthentication();
 
+
+        ConfigurationServices(services);
         builder.Services.AddDbContext<StoreDbContext>(options =>
         {
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
@@ -26,6 +24,24 @@ internal class Program
         Configure(app);
 
         app.Run();
+    }
+
+    private static void ConfigurationServices(IServiceCollection services)
+    {
+        services.AddControllers();
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
+        services.AddAuthorization();
+        services.AddAuthentication();
+        services.AddCors(options =>
+        {
+            options.AddPolicy("AllowLocalhost3000", builder =>
+            {
+                builder.WithOrigins("http://localhost:3000")
+                       .AllowAnyHeader()
+                       .AllowAnyMethod();
+            });
+        });
     }
     private static void Configure(WebApplication app)
     {
@@ -45,10 +61,11 @@ internal class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
+        app.UseCors("AllowLocalhost3000");
         app.UseHttpsRedirection();
 
         app.MapControllers();
+
     }
 
     private static async void RunInitialisers(WebApplication app)
